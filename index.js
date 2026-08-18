6d97335126a3c064f39fce38f554ca41994c3d58
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2026-08-18 18:51:41 +0530

    Other types of console log

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+process.stdout.write("This is a test message.");
+process.stdout.write("This is a test message.");
+
+// console.clear();
+
+console.warn("This is a warning message.");
+
+console.table([
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Charlie", age: 35 }
+]);
