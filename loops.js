d90c48495dcf757abd3bc98695cf780c7c86e194
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2026-08-20 19:30:50 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,18 @@
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log("For Loop Iteration:", i);
+}
+
+// while loop
+let j = 0;  
+while (j < 5) {
+    console.log("While Loop Iteration:", j);
+    j++;
+}
+
+// do-while loop
+let k = 9;
+do {
+    console.log("Do-While Loop Iteration:", k);
+    k++;
+} while (k < 5);
