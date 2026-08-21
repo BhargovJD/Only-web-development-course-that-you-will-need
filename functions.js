ffd72a625af8edcec033e4dfeb9ce66c78070c65
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2026-08-21 19:00:49 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+function myName() {
+    console.log("My name is John Doe");
+}
+myName();
+
+function addNumbers(a, b) {
+    return a + b;
+}
+const sum = addNumbers(5, 10);
+console.log("Sum of 5 and 10 is:", sum);
+
+// arrow function
+const multiplyNumbers = (x, y) => {
+    return x * y;
+}
+const product = multiplyNumbers(4, 6);
+console.log("Product of 4 and 6 is:", product);
